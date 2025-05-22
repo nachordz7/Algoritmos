365e527be5c5b3c21edfe44ba92e1f0a6b88bdf6
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-05-21 21:23:58 -0300

    Update and rename edad to edades.cpp

--- edades.cpp
+++ edades.cpp
@@ -1,7 +1,7 @@
 /* Realizar un programa que lea 10 edades y muestre el promedio entre todas las edades, el porcentaje de menores de edad y la edad maxima 
 */
 #include<stdio.h>
-int i, n=10, edad, cont=0, acum;
+int i, n=10, edad, cont=0, acum, ;
 float prom, porc;
 
 main(){
@@ -9,9 +9,14 @@ main(){
 	printf("Ingrese edad %d: ",i);
 	scanf("%d2",&edad);	
 	acum= acum+ edad;
+	if (edad<18) {
+		cont = cont +1;
+		
 	}
+	}
+	porc = (cont*100)/n ;
 	prom = acum/n;
 	printf("El promedio es de %f \n",prom);
-	//printf("El porcentaje de menores de edad es de: %f \n",cont);
+	printf("El porcentaje de menores de edad es de: %f \n",porc);
 	//printf("La edad maxima es: %d",max);
 }
