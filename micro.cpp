1d1ef1fb8357d522b692bb7bef4a5cb33a433815
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-05-27 20:04:00 -0300

    27/5

---
+++ micro.cpp
@@ -0,0 +1,26 @@
+/*
+Ingresan 5 pasajeros a un micro, se lee el el destino:
+1-Pinamar
+2-Villa Gesell
+3-Mar del plata
+
+Junto con la edad de cada pasajero.
+Informar
+1-Porcentaje de mayores de edad
+2-Que porcentaje fue a mar del plata
+3-Promedio de edad de los que viajan a Gesell
+4-Edad minima y a que destino se dirige
+*/
+
+#include<stdio.h>
+int n=5;
+int i;
+
+main(){
+for(i=0; i<n ; i++){
+	
+}
+	printf("Ingrese destino %d (1-Pinamar 2-Villa Gesell 3-Mar del plata): ",i);
+	scanf("%d",&n);
+}
+
