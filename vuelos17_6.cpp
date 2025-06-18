64640f40b51c8fa01dc6e4e8c15c30d7bc6aa181
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-06-18 15:44:38 -0300

    Update vuelos17_6.cpp

--- vuelos17_6.cpp
+++ vuelos17_6.cpp
@@ -4,18 +4,18 @@ Mostrar:
 1. Que porcentaje hace transbordo.
 2. Edad minima y si hacen transbordo o no.
 3. Promedio de edad entre los mayores a 50 que no hacen transbordo.
-4. Dentro de los menores de edad ¿Cuantos hacen transbordo?. Expresar en porcentaje  
+4. Dentro de los menores de edad Â¿Cuantos hacen transbordo?. Expresar en porcentaje  
 */
 #include<stdio.h>
 int i, n=5, cont=0, acum=0, prom=0;
-int edad, trans, min, transmin, transmayor, transmenor, contmenor=0;
+int edad, trans, min, transmin=0, transmayor=0, transmenor=0, contmenor=0;
 float porc=0, porcmenor=0;
 main(){
 	for(i=0; i<n; i++){
 		printf("\nIngrese edad %d: ",i);
 		scanf("%d",&edad);
 		
-		printf("Transbordo (1)si y (0)no: ",i);
+		printf("Transbordo (1)si y (0)no: ");
 		scanf("%d",&trans);
 		
 		if(trans==1){
@@ -35,23 +35,23 @@ main(){
 			if(trans == 1){
 				transmenor= transmenor + 1;
 			} 
-		}
 	}
+}
 	porc= (cont*100)/n;
-	prom= acum/transmayor;
-	if(contmenor >= 0){
-		porcmenor= (transmenor*100)/contmenor;
+	if(transmayor > 0){
+		prom= acum/transmayor;
+	} else{
+		prom = 0;
 	}
-	else{ porcmenor = 0;
+	
+	if(contmenor >0){
+		porcmenor= (transmenor*100)/contmenor;
+	} else{
+		porcmenor= 0;
 	}
 	
 	printf("\nPorcentaje que hace transbordo: %f%%\n",porc);
 	printf("Edad minima: %d\nTransbordo: %d\n",min,transmin);
 	printf("Promedio de edad entre los mayores a 50 anios que no hacen transbordo: %d\n",prom);
-	if(contmenor > 0){
-		printf("Porcentaje de los que hacen transbordo siendo menor de edad: %f%%",porcmenor);
-	}
-	else{
-		printf("No se registraron menores de edad.\n");
-	}
+	printf("Porcentaje de los que hacen transbordo siendo menor de edad: %f%%",porcmenor);	
 }
