5b36e9a6d92df0e16479930a39af441e0434d2fd
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-06-30 19:14:38 -0300

    Crear VECTORES/ notas.cpp

---
+++ VECTORES/ notas.cpp
@@ -0,0 +1,9 @@
+/*
+Se almacenan en un vector las notas de 4 evaluaciónes de un estudiante.Mostrar:
+1. Promedio. 
+2. Número de notas aprobadas. 
+3. Porcentaje de notas desaprobadas. 
+4. Numeros de notas mayores al promedio. 
+5. Nota maxima y posición de memoria. 
+El numero de nota corresponde a la posición de memoria
+*/
