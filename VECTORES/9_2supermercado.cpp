0ac9848353c54dabf845dce76f10dd53055a711e
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-09-03 21:24:54 -0300

    Update 9_2supermercado.cpp

--- VECTORES/9_2supermercado.cpp
+++ VECTORES/9_2supermercado.cpp
@@ -5,3 +5,26 @@ El usario indica si desa cargar otro producto.
 Imprimir el total a pagar. Por lo menos se lee un articulo.
 HACER CON DO WHILE
 */
+#include <stdio.h>
+float precio, total;
+int cant;
+int opcion;
+main(){
+    printf("Carga de productos para el ticket del supermercado\n");
+
+    do {
+        printf("Ingrese el precio del producto: ");
+        scanf("%f", &precio);
+
+        printf("Ingrese la cantidad: ");
+        scanf("%d", &cant);
+
+        total += precio * cant;
+
+        printf("\nDesea ingresar otro producto? (1=Sí, 0=No): ");
+        scanf("%d", &opcion);
+
+    } while(opcion == 1);
+
+    printf("\nTotal a pagar: %2.f$\n", total);
+}
