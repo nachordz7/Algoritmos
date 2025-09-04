74292405d2ae9cb890a6b04d89d1c59c3687d6bc
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-09-03 22:37:58 -0300

    Add files via upload

---
+++ VECTORES/9_3curso.cpp
@@ -0,0 +1,37 @@
+/* 
+Se desea procesar las notas de estudiantes de 2 cursos. 
+Se lee el curso que pertenece a cada estudiante, su edad y su nota.Mostrar:
+1. Edad del estudiante que tuvo la menor nota y a que curso pertenece.
+2. Porcentaje de estudiantes mayores de edad por curso.
+3. Nota promedio entre los menores de edad.
+Rinde como maximo 5 estudiantes. Finaliza el programa al ingresar 0 como curso. Puede que no se presente ningun alumno.
+*/
+#include<stdio.h>
+int i, n, curso, edad, nota, cont;
+int min, edad_min, curso_min;
+main(){
+	printf("\nIndique curso 1 o 2: ");
+		scanf("%d",&curso);
+	while(curso!=0 &&cont<5){
+		
+		
+		printf("Ingrese la edad del alumno: ");
+		scanf("%d",&edad);
+		
+		printf("Ingrese nota: ");
+		scanf("%d",&nota);
+	
+	
+		
+		if(nota<min){ 
+			min = nota;
+			edad_min= edad;
+			curso_min = cont;
+		}
+		cont++;
+	};
+	
+	printf("\nEdad del estudiante que tuvo la menor nota: %d\tCurso: %d",edad_min,curso_min);
+	
+}
+
