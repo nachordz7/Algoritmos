8e4293d633bfecd6c5e0defac5d3338db22b949a
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-09-09 20:50:27 -0300

    Add files via upload

---
+++ VECTORES/9_9censo.cpp
@@ -0,0 +1,11 @@
+/*
+Para procesar los datos de un censo, realizar un programa que lea los datos de 8 personas maximo. 
+Se leen de cada persona su genero y edad. Puede ser que no se procese datos. 
+Finaliza el progama al ingresar 0 como edad
+Mostrar:
+1. Porcentaje de cada genero.
+2. Edad menor masculina y edad mayor entre todos los censados.
+3. Promedio de las edades entre todos los censados y promedio de edades masculinas.
+4. Que porcentaje entre las mujeres corresponde a menores de 40 años.
+5. Entre los mayores a 50 que porcentaje son hombres.
+*/
