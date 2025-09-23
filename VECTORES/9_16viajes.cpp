dddd0e85d63f39ad01984445ec25bc2315f4a75b
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-09-23 19:49:25 -0300

    Crear 9_16viajes.cpp

---
+++ VECTORES/9_16viajes.cpp
@@ -0,0 +1,8 @@
+/*
+Realizar un programa que procese los viajes de un remis, se lee el monto del viaje y si el pago fue hecho en efectivo o por medios digitales. Puede ser que no se realicen viajes. Finaliza el programa al ingresar 0 como monto. Mostrar:
+1. Porcentaje de viajes con monto mayor a 10000
+2. Precio promedio de los viajes pagados con tarjeta
+3. Precio del viaje mas barato y porque medio se abono
+4. Precio promedio entre todos los viajes
+5. Dentro de los viajes con monto mayor a 10000, ¿que cantidad pago en efectivo? Expresar en porcentaje
+*/
