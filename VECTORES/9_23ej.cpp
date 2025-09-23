123cdd2ccffe4fe9aaa6ff8cfe441f039fb35f83
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-09-23 20:54:24 -0300

    Crear 9_23ej.cpp

---
+++ VECTORES/9_23ej.cpp
@@ -0,0 +1,9 @@
+/*
+Se desean procesar los datos de los vehículos de un taller. Mostrar:
+1. Porcentaje de autos comerciales que asistieron por reparaciones. 
+2. valor promedio de la atención a autos particulares. 
+3. Costo del ingreso más barato y a que tipo de vehículo fue. 
+4. Dentro de los ingresos que superaron los 150k, cuantos corresponden a controles, expresar en porcentaje.
+Por cada, cliente se leen el precio del servicio si es auto particular o comercial y si asiste por 1 control o 2 reparación. Puede ser que no haya clientes. Finaliza la carga al ingresar 0 como precio del servicio y como máximo se leen los datos de 5 vehículos por día. 
+
+*/
