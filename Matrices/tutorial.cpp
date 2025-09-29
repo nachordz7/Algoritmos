1aa8925c77cf47a98fced6b7428061dea2b2ff94
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-09-29 19:04:14 -0300

    Actualizar tutorial.cpp

--- Matrices/tutorial.cpp
+++ Matrices/tutorial.cpp
@@ -3,5 +3,7 @@ int matriz[3][3], i, j, n=3;
 
 main() {
 For(i=0; i<n; i++) {
-For(j=0; j<n; j++) {
-printf ("Ingrese valor para la fila %d columna %d: ", i, j) ;
+ For(j=0; j<n; j++) {
+ printf ("Ingrese valor para la fila %d columna  %d:  ", i, j) ;
+ scanf ("%d", &matriz[i][j]) ;
+}
