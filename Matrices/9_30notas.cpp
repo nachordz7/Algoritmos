8f224b21478d15dcc00546d95a2dc7b3a45bbde9
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-09-30 21:02:50 -0300

    Update 9_30notas.cpp

--- Matrices/9_30notas.cpp
+++ Matrices/9_30notas.cpp
@@ -7,12 +7,32 @@ Mostrar:
 4.Minimo valor de la matriz y su posicion de memoria
 */
 #include<stdio.h>
-int matriz[3][3], i, j, n = 3;
+int notas[3][3], i, j, n = 3;
 int acum_est, acum;
 float prom_est, prom;
 main() {
-
-
-
-
-}
+	for(i=0; i<n; i++){
+		printf("\n");
+		for(j=0; j<n; j++){
+			printf("Ingrese nota del estudiante %d de la prueba %d: ",i,j);
+			scanf("%d",&notas[i][j]);
+						
+			acum_est = acum_est + notas[i][j];
+		}
+		
+	}
+	prom_est = acum_est/n;
+	
+	
+	for(i=0;i<n;i++){
+		printf("\n");
+		for(j=0;j<n;j++){
+			printf("%d |",notas[i][j]);
+		}
+	}
+	
+	for(i=0; i<n; i++){
+		
+		printf("\nPromedio de nota del estudiante %d: %f\n",i,prom_est);
+	}
+	}
