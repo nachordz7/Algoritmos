090a39a7a4a5c427295044840bc3a37589938d1a
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-10-14 20:54:56 -0300

    Update 10_13subte.cpp

--- Matrices/10_13subte.cpp
+++ Matrices/10_13subte.cpp
@@ -10,6 +10,7 @@ Mostrar:
 int matriz[3][3], i, j, n=3;
 int acum_est, max, est_max;
 int acum_dia, cont_dia;
+int diag;
 float prom_est, prom_dia, porc_dia;
 main(){
 	for(i=0;i<n;i++){
@@ -52,5 +53,15 @@ main(){
 		}
 		porc_dia = (cont_dia*100)/n;
 		printf("\nPorcentaje de dias que usaron el servicio mas de 300 personas: %f%%\n",porc_dia);
-	}
 	
+	//3.
+	printf("Diagonal principal:\n");
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			if(i==j){
+				diag = matriz[i][j];
+			}
+		}
+		printf("%d |",diag);
+	}
+	}
