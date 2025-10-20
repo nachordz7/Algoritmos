6448b08fd971f38c29b4b2100f2c871a71279b55
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-10-20 18:47:53 -0300

    Crear 10_20aerolinea.cpp

---
+++ Matrices/10_20aerolinea.cpp
@@ -0,0 +1,7 @@
+/*
+Una aerolinea registra 3 viajes a 3 destinos. Mostrar:
+1. total de personas por destino y que destino tuvo menos visitantes. 
+2. Promedio de pasajeros por día y cuantos días volaron más de 500 personas. (expresar en porcentaje). 
+3. Maximo valor de la matriz, posicion de la memoria y traza. 
+4. En cada dia mostrar que destino fue menos concurrido. 
+*/
