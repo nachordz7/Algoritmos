61e86ac8f2c8fcb015eb15f970de75367f123cbd
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-10-21 19:38:55 -0300

    Update 10_20aerolinea.cpp

--- Matrices/10_20aerolinea.cpp
+++ Matrices/10_20aerolinea.cpp
@@ -1,7 +1,66 @@
 /*
-Una aerolinea registra 3 destinos en 3 dias. Mostrar:
+Una aerolinea registra las personas que viajan a 3 destinos durante 3 dias. Mostrar:
 1. total de personas por destino y que destino tuvo menos visitantes. 
-2. Promedio de pasajeros por día y cuantos días volaron más de 500 personas. (expresar en porcentaje). 
+2. Promedio de pasajeros por dia y cuantos dias volaron mas de 500 personas. (expresar en porcentaje). 
 3. Maximo valor de la matriz, posicion de la memoria y traza. 
 4. En cada dia mostrar que destino fue menos concurrido. 
 */
+#include <stdio.h>
+int pasa[3][3], i, j, N=3, acum=0;
+float prom;
+main(){
+	//CARGA
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			printf("Ingrese pasajeros al destino %d en el dia %d ", i,j);
+			scanf("%d", &pasa[i][j]);
+		}
+	}
+	// 1.personas por destino (filas)
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			acum=acum+pasa[i][j];
+		}
+		printf("Total de personas del destino %d es %d \n", i, acum);
+		acum=0;
+		
+	}
+
+    // 2.promedio por dia (columnas)
+	for(j=0; j<N; j++){
+		for(i=0; i<N; i++){
+			acum=acum+pasa[i][j];
+		}
+		prom=acum/N;
+		printf("El promedio de personas del dia %d es %f \n", j, prom);
+		acum=0;
+		
+	}
+	
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			if( (i==0 && j==0)|| pasa[i][j]>max ){
+				max=pasa[i][j];
+				pos_i=i;
+				pos_j=j;
+			}
+			if(i==j){
+				traza=taza+pasa[i][j];
+			}
+		}		
+	}
+	printf("El maximo es y se encuenta en %d i= %d j= %d \n", max, pos_i, pos_j);
+	printf("La traza es %d", traza);
+	
+	for(j=0; j<N; j++){
+		for(i=0; i<N; i++){
+			if((i==0)|| pasa[i][j]<min ){
+				min=pasa[i][j];
+				dia_min=i;
+			}
+		}		
+		printf("El dia %d hubo menos pasajeros\n",dia_min);
+	
+	}
+	
+}
